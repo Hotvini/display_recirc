52d1ba543b478bf12cd8a67df2ced72cad95d52e
viniciuskom <vinicius.andrade@komlog.com.br>
2026-02-20 11:26:11 -0300

    gitignore

--- armgcc/debug/CMakeFiles/clean_additional.cmake
+++ armgcc/debug/CMakeFiles/clean_additional.cmake
@@ -3,7 +3,7 @@ cmake_minimum_required(VERSION 3.16)
 
 if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "debug")
   file(REMOVE_RECURSE
-  "new_project.bin"
   "output.map"
+  "recirc_display.bin"
   )
 endif()
